2449cdf6ba19b72999543931a00c7caae1fba8d2
gohsk <gohsk@LAPTOP-6LAMO0L1>
2023-08-11 09:54:55 +0800

    commit by richard, grade class new

--- C206_CaseStudy/src/Grade.java
+++ C206_CaseStudy/src/Grade.java
@@ -1,30 +1,42 @@
 
 public class Grade {
-	
-	private String name;
+	private String sID;
+	private String cID;
 	private String grade;
-	
-	public Grade(String name, String grade) {
-		this.name = name;
+
+	public Grade(String sID, String cID, String grade) {
+		this.sID = sID;
+		this.cID = cID;
 		this.grade = grade;
 	}
+
+	public String getStudentID() {
+		return sID;
+	}
+
+	public void setStudentID(String sID) {
+		this.sID = sID;
+	}
+
+	public String getCourseID() {
+		return cID;
+	}
+
+	public void setCourseID(String cID) {
+		this.cID = cID;
+	}
+
 	public String getGrade() {
 		return grade;
 	}
+
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+
 	public void display() {
-		System.out.println("\nStudent Name: " + name);
+		System.out.println("\nStudent ID: " + sID);
+		System.out.println("Course ID: + cID");
 		System.out.println("Student Grade: " + grade);
 	}
-	
 }
-
