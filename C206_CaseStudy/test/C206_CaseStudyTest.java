d273f90cedce7e7f90e7fea6c9061fefa6dbb809
khow2 <khow2@10.175.7.35>
2023-08-11 12:37:58 +0800

    commit by richard

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -11,9 +11,12 @@ public class C206_CaseStudyTest {
 	private Student s2;
 	private Enrolment e1;
 	private Enrolment e2;
+	private Fee f1;
+	private Fee f2;
 	
 	private ArrayList<Student> studentList;
 	private ArrayList<Enrolment> enrolmentList;
+	private ArrayList<Fee> feeList;
 
 	@Before
 	public void setUp() throws Exception {
@@ -22,9 +25,12 @@ public class C206_CaseStudyTest {
 		s2 = new Student("22021234","skye");
 		e1 = new Enrolment("22005047", "C110");
 		e2 = new Enrolment("22028513", "C123");
+		f1 = new Fee("22028513", "Exam", 70.0);
+		f2 = new Fee("22021234", "Tuition", 100.0);
 		
 		studentList = new ArrayList<Student>();
 		enrolmentList = new ArrayList<Enrolment>();
+		feeList = new ArrayList<Fee>();
 	}
 	
 	
@@ -44,6 +50,43 @@ public class C206_CaseStudyTest {
 	public void testDelete() {
 		//......
 	}
+	
+	@Test
+	public void testAddFee() {
+		// Item list is not null, so that can add a new item - boundary
+		assertNotNull("Test if there is valid Fee arraylist to add to", feeList);
+		
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		C206_CaseStudy.addFee(feeList, f1);		
+		assertEquals("Test that Fee arraylist size is 1", 1, feeList.size());
+		assertSame("Test that Fee is added", f1, feeList.get(0));
+		
+		//Add another item. test The size of the list is 2? - normal
+		//The item just added is as same as the second item of the list
+		C206_CaseStudy.addFee(feeList, f2);
+		assertEquals("Test that Fee arraylist size is 2", 2, feeList.size());
+		assertSame("Test that Fee is added", f2, feeList.get(1));
+	}
+	
+	@Test
+	public void testDeleteFee() {
+		// Item list is not null, so that can delete a fee - boundary
+		assertNotNull("Test if there is valid Fee arraylist to delete from", feeList);
+		
+		//Given a list with two fees, after deleting 1 fee, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		C206_CaseStudy.deleteFee(feeList, f1);		
+		assertEquals("Test that Fee arraylist size is 2", 2, feeList.size());
+		assertSame("Test that Fee is deleted", f1, feeList.get(0));
+		
+		//Add another item. test The size of the list is 2? - normal
+		//The item just added is as same as the second item of the list
+		C206_CaseStudy.addFee(feeList, f2);
+		assertEquals("Test that Fee arraylist size is 2", 2, feeList.size());
+		assertSame("Test that Fee is added", f2, feeList.get(1));
+	}
+
 	@Test
 	public void testAddEnrolment() {
 		// Item list is not null, so that can add a new item - boundary
